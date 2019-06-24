c133dc2e1b35c082fca0f2ddd490e59c4d4270f0
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2019-06-24 04:16:57 +0300

    Added a check for when the pathfinding fails to find a path.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use pathfinding::prelude::{absdiff, astar};
-use std::time::{Duration, Instant};
+use std::time::Instant;
+use std::collections::HashMap;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos(usize, usize);
@@ -131,24 +132,34 @@ fn debug_path(grid: Vec<Vec<u32>>, start_x: usize, start_y: usize, x: usize, y: 
 
 #[pyfunction]
 /// Find the path and returns the path
-fn find_path(grid: Vec<Vec<u32>>, start_x: usize, start_y: usize, x: usize, y: usize) -> PyResult<(Vec<(usize, usize)>, u32)> {
-    let start: Pos = Pos(start_x, start_y);
-    let goal: Pos = Pos(x, y);
-    let result = astar(&start, |p| p.successors(&grid), |p| p.distance(&goal) / 3, |p| *p == goal);
-    //assert_eq!(result.expect("no path found").len(), 5);
-    let unwrapped = result.unwrap();
-    let mut path = Vec::<(usize, usize)>::with_capacity(unwrapped.0.len());
+fn find_path(grid: Vec<Vec<u32>>, start: (usize, usize), end: (usize, usize)) -> PyResult<(Vec<(usize, usize)>, u32)> {
+    let start: Pos = Pos(start.0, start.1);
+    let goal: Pos = Pos(end.0, end.1);
 
-    for pos in unwrapped.0 {
-        path.push((pos.0, pos.1))    
+    let result = astar(&start, |p| p.successors(&grid), |p| p.distance(&goal), |p| *p == goal);
+    
+    let mut path: Vec<(usize, usize)>;
+    let distance: u32;
+
+    if result.is_none(){
+        path = Vec::<(usize, usize)>::new();
+        distance = 0
     }
-    Ok((path, unwrapped.1))
+    else {
+        let unwrapped = result.unwrap();
+        distance = unwrapped.1;
+        path = Vec::<(usize, usize)>::with_capacity(unwrapped.0.len());
+        for pos in unwrapped.0 {
+            path.push((pos.0, pos.1))    
+        }
+    }
+    
+    Ok((path, distance))
 }
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-fn sc2pathlib(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+fn sc2pathlib(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(debug_path))?;
     m.add_wrapped(wrap_pyfunction!(find_path))?;
 
