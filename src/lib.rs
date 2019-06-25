531350cf05a86b666fa3c6f666e4bf77b6d49ea3
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2019-06-25 23:54:04 +0300

    Made distance heuristics their own methods and made it optional to use influence which should improve the performance a little bit.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use pathfinding::prelude::{absdiff, astar, dijkstra_all};
 use std::time::Instant;
-use std::collections::HashMap;
 use std::cmp::min;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -10,21 +9,25 @@ struct Pos(usize, usize);
 //static SQRT2: f32 = 1.4142135623730950488016887242097;
 static SQRT2: usize = 14142;
 static MULT: usize = 10000;
-static MULTf64: f64 = 10000.0;
+static MULTF64: f64 = 10000.0;
 
 impl Pos {
-    fn distance(&self, other: &Pos) -> usize {
-        //((((self.0 - other.0).pow(2) + (self.1 - other.1).pow(2)) as f64).sqrt() * MULTf64) as usize 
-
+    fn manhattan_distance(&self, other: &Pos) -> usize {
         (absdiff(self.0, other.0) + absdiff(self.1, other.1)) * MULT
-
-        //let xd = absdiff(self.0, other.0);
-        //let yd = absdiff(self.1, other.1);
-        //let diag = min(xd, yd);
-        //(xd + yd - diag - diag) * MULT + diag * SQRT2
     }
 
-  fn successors(&self, grid: &Vec<Vec<usize>>) -> Vec<(Pos, usize)> {
+    fn euclidean_distance(&self, other: &Pos) -> usize {
+        ((((self.0 - other.0).pow(2) + (self.1 - other.1).pow(2)) as f64).sqrt() * MULTF64) as usize
+    }
+
+    fn quick_distance(&self, other: &Pos) -> usize {
+        let xd = absdiff(self.0, other.0);
+        let yd = absdiff(self.1, other.1);
+        let diag = min(xd, yd);
+        (xd + yd - diag - diag) * MULT + diag * SQRT2
+    }
+
+    fn successors(&self, grid: &Vec<Vec<usize>>) -> Vec<(Pos, usize)> {
         let &Pos(x, y) = self;
         let mut arr = Vec::<(Pos, usize)>::with_capacity(8);
         //let arr = Vec<(Pos, f32)>();
@@ -50,12 +53,10 @@ impl Pos {
             val_up = grid[x][y + 1];
         }
 
-        if val_left > 0
-        {
+        if val_left > 0 {
             arr.push((Pos(x - 1, y), val_left * MULT));
 
-            if val_down > 0
-            {
+            if val_down > 0 {
                 let diag_val = grid[x - 1][y - 1];
 
                 if diag_val > 0 {
@@ -63,8 +64,7 @@ impl Pos {
                 }
             }
 
-            if val_up > 0
-            {
+            if val_up > 0 {
                 let diag_val = grid[x - 1][y + 1];
 
                 if diag_val > 0 {
@@ -73,12 +73,10 @@ impl Pos {
             }
         }
 
-        if val_right > 0
-        {
+        if val_right > 0 {
             arr.push((Pos(x + 1, y), val_right * MULT));
 
-            if val_down > 0
-            {
+            if val_down > 0 {
                 let diag_val = grid[x + 1][y - 1];
 
                 if diag_val > 0 {
@@ -86,8 +84,7 @@ impl Pos {
                 }
             }
 
-            if val_up > 0
-            {
+            if val_up > 0 {
                 let diag_val = grid[x + 1][y + 1];
 
                 if diag_val > 0 {
@@ -96,14 +93,89 @@ impl Pos {
             }
         }
 
-        if val_up > 0
-        {
+        if val_up > 0 {
             arr.push((Pos(x, y + 1), val_up * MULT));
         }
 
-        if val_down > 0
-        {
+        if val_down > 0 {
             arr.push((Pos(x, y - 1), val_down * MULT));
+        }
+
+        arr
+    }
+
+    fn successors_no_influence(&self, grid: &Vec<Vec<usize>>) -> Vec<(Pos, usize)> {
+        let &Pos(x, y) = self;
+        let mut arr = Vec::<(Pos, usize)>::with_capacity(8);
+        //let arr = Vec<(Pos, f32)>();
+
+        let mut val_left: usize = 0;
+        let mut val_down: usize = 0;
+        let mut val_right: usize = 0;
+        let mut val_up: usize = 0;
+
+        if x > 0 {
+            val_left =  grid[x - 1][y];
+        }
+
+        if y > 0 {
+            val_down = grid[x][y - 1];
+        }
+
+        if x + 1 < grid.len() {
+            val_right = grid[x + 1][y];
+        }
+
+        if y + 1 < grid[0].len() {
+            val_up = grid[x][y + 1];
+        }
+
+        if val_left > 0 {
+            arr.push((Pos(x - 1, y), MULT));
+
+            if val_down > 0 {
+                let diag_val = grid[x - 1][y - 1];
+
+                if diag_val > 0 {
+                    arr.push((Pos(x - 1, y - 1), SQRT2));
+                }
+            }
+
+            if val_up > 0 {
+                let diag_val = grid[x - 1][y + 1];
+
+                if diag_val > 0 {
+                    arr.push((Pos(x - 1, y + 1), SQRT2));
+                }
+            }
+        }
+
+        if val_right > 0 {
+            arr.push((Pos(x + 1, y), val_right * MULT));
+
+            if val_down > 0 {
+                let diag_val = grid[x + 1][y - 1];
+
+                if diag_val > 0 {
+                    arr.push((Pos(x + 1, y - 1), SQRT2));
+                }
+            }
+
+            if val_up > 0 {
+                let diag_val = grid[x + 1][y + 1];
+
+                if diag_val > 0 {
+                    arr.push((Pos(x + 1, y + 1), SQRT2));
+                }
+            }
+        }
+
+        if val_up > 0 {
+            arr.push((Pos(x, y + 1), MULT));
+        }
+
+        if val_down > 0 {
+            arr.push((Pos(x, y - 1), MULT));
         }
 
         arr
@@ -113,56 +185,53 @@ impl Pos {
 #[pyclass]
 pub struct PathFind {
     map: Vec<Vec<usize>>,
+    dict: Vec<Vec<Vec<(Pos, usize)>>>
 }
 
 #[pymethods]
 impl PathFind {
     #[new]
     fn new(obj: &PyRawObject, map: Vec<Vec<usize>>) {
-        obj.init(PathFind { map })
+        let mut dict : Vec<Vec<Vec<(Pos, usize)>>> = Vec::<Vec<Vec<(Pos, usize)>>>::new();
+        let height = map[0].len();
+
+        for x in 0..map.len() {
+            let mut column = Vec::<Vec<(Pos, usize)>>::new();
+
+            for y in 0..height {
+                let temp = Pos(x, y);
+                let successors = temp.successors(&map);
+                column.push(successors)
+            }
+
+            dict.push(column);
+        }
+
+        obj.init(PathFind { map, dict })
     }
 
-    /// Tests a path and returns a string defining the tested path
-    fn debug_path(&mut self, start: (usize, usize), end: (usize, usize)) -> PyResult<String> {
+    /// Find the path using influence values and returns the path and distance
+    fn find_path(&self, start: (usize, usize), end: (usize, usize), 
+            possible_influence: Option<bool>, possible_heuristic: Option<u8>) -> PyResult<(Vec<(usize, usize)>, f64)> {
         let start: Pos = Pos(start.0, start.1);
         let goal: Pos = Pos(end.0, end.1);
         let grid: &Vec<Vec<usize>> = &self.map;
-        
-        let mut dict = HashMap::<Pos,Vec<(Pos, usize)>>::new();
-        let height = grid[0].len();
+        let dict: &Vec<Vec<Vec<(Pos, usize)>>> = &self.dict;
+        let use_influence = possible_influence.unwrap_or(true);
 
-        for x in 0..grid.len() {
-        for y in 0..height {
-            let temp = Pos(x, y);
-            let successors = temp.successors(&grid);
-            dict.insert(temp, successors);
-        }
-        }
-        let test = dict.get(&start).unwrap();
 
-        let now = Instant::now();
-        let result = astar(&start, |p| dict.get(&p).unwrap().clone(), |p| p.distance(&goal), |p| *p == goal);
-        let time_taken = now.elapsed().as_micros() as f32 / 1000.0;
+        let method: Box<Fn(&Pos) -> Vec<(Pos, usize)>> = match use_influence {
+            true => Box::new(|p: &Pos| p.successors(&grid)),
+            _ => Box::new(|p: &Pos| p.successors_no_influence(&grid))
+        };
 
-        let unwrapped = result.unwrap();
-        let path = unwrapped.0;
-        let distance = unwrapped.1;
+        let heuristic: Box<Fn(&Pos)->usize> = match possible_heuristic.unwrap_or(0) {
+            0 => Box::new(|p: &Pos| p.manhattan_distance(&goal)),
+            1 => Box::new(|p: &Pos| p.quick_distance(&goal)),
+            _ => Box::new(|p: &Pos| p.euclidean_distance(&goal)),
+        };
 
-        let steps = path.len().to_string();
-        let mut path_text = String::new();
-        for step in path {
-            path_text.push_str(&format!("{},{} ", step.0, step.1));
-        }
-        Ok(format!("time taken: {} ms len: {} distance: {} start: {},{} goal: {},{} Path: {}", time_taken, steps, distance, start.0, start.1, end.0, end.1, &path_text))
-    }
-
-    /// Find the path and returns the path
-    fn find_path(&mut self,start: (usize, usize), end: (usize, usize)) -> PyResult<(Vec<(usize, usize)>, f64)> {
-        let start: Pos = Pos(start.0, start.1);
-        let goal: Pos = Pos(end.0, end.1);
-        let grid: &Vec<Vec<usize>> = &self.map;
-        
-        let result = astar(&start, |p| p.successors(&grid), |p| p.distance(&goal), |p| *p == goal);
+        let result = astar(&start, method, heuristic, |p| *p == goal);
         
         let mut path: Vec<(usize, usize)>;
         let distance: f64;
@@ -173,7 +242,7 @@ impl PathFind {
         }
         else {
             let unwrapped = result.unwrap();
-            distance = (unwrapped.1 as f64) / MULTf64;
+            distance = (unwrapped.1 as f64) / MULTF64;
             path = Vec::<(usize, usize)>::with_capacity(unwrapped.0.len());
             for pos in unwrapped.0 {
                 path.push((pos.0, pos.1))    
@@ -185,7 +254,7 @@ impl PathFind {
 
     /// Finds all reachable destinations from selected start point. Ignores influence.
     fn find_all_destinations(&self, start: (usize, usize)) -> PyResult<Vec<((usize, usize), f64)>> {
-    let start: Pos = Pos(start.0, start.1);
+        let start: Pos = Pos(start.0, start.1);
         let grid: &Vec<Vec<usize>> = &self.map;
         let result = dijkstra_all(&start, |p| p.successors_no_influence(&grid));
 
