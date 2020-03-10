de0853765358d643b0a4aafb4e9af610b8bddf81
eladyaniv01 <40754127+eladyaniv01@users.noreply.github.com>
2020-03-10 18:32:26 +0200

    Better Debugging for newcommers
    * uncomment `point_array` code block  to plot the text maze solution 
    * will now plot all paths on AutomatonLE (one after the other)

--- test.py
+++ test.py
@@ -18,9 +18,36 @@ def read_maze(file_name: str) -> List[List[int]]:
 
 maze = read_maze("tests/maze4x4.txt")
 pf = sc2pathlibp.PathFinder(maze)
-print(pf.map)
+for row in pf.map: # just pretty printing the matrix 
+    print(row)
+
 print(pf.width)
 print(pf.height)
+
+p0 = (0, 0)
+p1 = (0, 2)
+path = pf.find_path(p0, p1)
+print(f"pf.find_path(p0, p1) : {path}")
+
+# uncomment for basic text plotting of the path
+# point_array = pf.map.copy()
+# point_array[0][0] = 'S' #start
+# point_array[0][2] = 'E' #end
+# print(f"finding path from {p0} -> {p1}")
+# for i in range(len(point_array)):
+#     for j in range(len(point_array[0])):
+#         point_array[i][j] = " " + str(point_array[i][j]) + " "
+# for row in point_array:
+#     print(row)
+# for i, cell in enumerate(path[0]):
+#     point_array[cell[0]][cell[1]] = "_" + str(i) + "_"
+# for row in point_array:
+#     print(row)
+
+
+pf.normalize_influence(100)
+print(pf.lowest_influence_in_grid((2, 2), 5))
+print(pf.find_path((0, 0), (0, 2)))
 
 print(pf.find_path((0, 0), (0, 2)))
 pf.normalize_influence(100)
@@ -69,9 +96,12 @@ ns_pf = time.perf_counter_ns()
 pf.normalize_influence(100)
 pf.heuristic_accuracy = 2
 
+results = []
+
 for pos1 in expansions:
     for pos2 in expansions:
         result = pf.find_path(pos1, pos2, False)
+        results.append(result)
         total_distance += result[1]
 
 ns_pf = time.perf_counter_ns() - ns_pf
@@ -91,11 +121,11 @@ pf.add_influence_walk([(56, 65), (110, 28), (100, 98)], 150, 10, False)
 ns_pf = time.perf_counter_ns() - ns_pf
 print(f"adding influence by walking distance took {ns_pf / 1000 / 1000} ms.")
 
-result = pf.find_path_influence((29, 65), (154, 114))
+results.append(pf.find_path_influence((29, 65), (154, 114)))
 # print(pf.map)
 # pf.reset()
 # pf.normalize_influence(100)
-pf.plot(result[0])
+[pf.plot(result[0]) for result in results]
 pf.create_block([(11.5, 11.5), (21.5, 21.5), (31.5, 31.5), (31.5, 31.5)], (2, 1))
 pf.plot(result[0])
 input("Press Enter to continue...")
